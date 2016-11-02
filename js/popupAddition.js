9f2f4fd39109627f4da160fe2ee4cceef2667d77
Austin Carrig <austincarrig@gmail.com>
2016-11-02 16:08:28 -0400

    Major edits to peope page, integration of new photos.

---
+++ js/popupAddition.js
@@ -0,0 +1,5 @@
+$(function () {
+    // Add html in footerElement.html to footer elements in
+    // the .html files where templateAddition.js is added
+    $(".popup").load("TemplateElements/popupTemplate.html");
+});
